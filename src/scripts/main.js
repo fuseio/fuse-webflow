d2c84c9e08fe0af3dcfce1e83fa5cb0d0a14f8aa
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-09-10 15:55:17 +0000

    Add animate summary tab in network page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -565,4 +565,19 @@ $(function () {
     }
   }
   moreSuccessStories();
+
+  function animateJoinImage() {
+    const leftImages = $(".join-image-wrapper.left");
+    const rightImages = $(".join-image-wrapper.right");
+    leftImages.css({ left: "-25%" });
+    rightImages.css({ right: "-25%" });
+
+    const totalImages = 12;
+    let decimal = 0.25;
+    for (let i = 1; i <= totalImages; i++) {
+      const currDelay = i * decimal;
+      $(`.join-image_${i}`).css("animation-delay", `${currDelay}s`);
+    }
+  }
+  animateJoinImage();
 });
