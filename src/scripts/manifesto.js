c95524a3026bf2882ec18c0547cfdb409bf8df29
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-11-07 15:24:14 +0000

    Add custom scroll snap in Manifesto

--- src/scripts/manifesto.js
+++ src/scripts/manifesto.js
@@ -1,170 +1,41 @@
-import { safeExecute, isMediumScreen } from '../utils/helper';
+import { safeExecute } from '../utils/helper';
 
-function animateFundamental() {
-  const box = document.querySelector('.manifesto_box.is-center');
-  const boxLeft = document.querySelector('.manifesto_box.is-left');
-  const boxRight = document.querySelector('.manifesto_box.is-right');
-  const container = box.querySelector('.manifesto_box-container');
-  const sliders = document.querySelectorAll('.manifesto_box-wrapper.is-slider');
-  const fuse = document.querySelector('.manifesto_box-image.is-fuse');
-  const token = document.querySelector('.manifesto_box-image.is-tokens');
-  const titleContainer = document.querySelector('.manifesto_fundamental-title-container');
-  const titleWrapper = document.querySelector('.manifesto_fundamental-title-wrapper');
-  const bullets = document.querySelectorAll('.manifesto_hero-bullet');
+function fullPageScroll() {
+  gsap.registerPlugin(Observer, ScrollToPlugin);
 
-  const containerWidth = container.getBoundingClientRect().width;
-  const containerHeight = container.getBoundingClientRect().height;
-  let scrollY = 0;
-  let isSideBoxesRevealed = false;
-  let currentBox = 1;
-  const backgroundGradients = {
-    red: 'linear-gradient(#f65d51,#f32f20)',
-    green: 'linear-gradient(180deg, #BAFEC0 0%, #0F8519 100%)'
-  }
+  const sections = document.querySelectorAll('.main-wrapper .container section');
+  let currentIndex = 0;
+  let animating = false;
+  let delay = 0
 
-  const firstScrolled = containerHeight;
-  const secondScrolled = firstScrolled + containerWidth;
-  const thirdScrolled = secondScrolled + containerWidth;
-  const fourthScrolled = thirdScrolled + containerHeight;
-  const fifthScrolled = fourthScrolled + containerHeight;
-  const sixthScrolled = fifthScrolled + window.innerHeight;
-  const scrollMap = {
-    7: sixthScrolled,
-    6: fifthScrolled,
-    5: fourthScrolled,
-    4: thirdScrolled,
-    3: secondScrolled,
-    2: firstScrolled,
-    1: 0
-  };
-  const sortedScrollMap = Object.keys(scrollMap).sort((a, b) => scrollMap[b] - scrollMap[a]);
+  function gotoSection(index) {
+    if (index < 0 || index >= sections.length || animating || !sections[index] || Date.now() - delay < 400) return;
 
-  const titleHeight = titleWrapper.getBoundingClientRect().height;
-
-  function disappearSideBoxes() {
-    isSideBoxesRevealed = false;
-
-    boxLeft.style.transform = `rotateY(-15deg) translate(-300%, -50%) scale(0)`;
-    boxRight.style.transform = `rotateY(15deg) translate(250%, -50%) scale(0)`;
-    boxRight.style.left = `0%`;
-  }
-
-  function revealSideBoxes() {
-    if (isSideBoxesRevealed) return;
-    isSideBoxesRevealed = true;
-
-    boxLeft.style.transform = `rotateY(-15deg) translate(-220%, -50%) scale(1)`;
-    boxRight.style.transform = `rotateY(15deg) translate(170%, -50%) scale(1)`;
-    boxRight.style.left = `0%`;
-  }
-
-  function hideSideBoxes() {
-    if (!isSideBoxesRevealed) return;
-    isSideBoxesRevealed = false;
-
-    boxLeft.style.transform = `rotateY(0deg) translate(-50%, -50%) scale(1)`;
-    boxRight.style.transform = `rotateY(0deg) translate(-50%, -50%) scale(1)`;
-    boxRight.style.left = `50%`;
-  }
-
-  function revealTokens() {
-    fuse.style.transform = `translate(-50%, 0%) scale(0.5)`;
-    token.style.transform = `translate(-50%, 1rem) scale(1.2)`;
-  }
-
-  function hideTokens() {
-    fuse.style.transform = `translate(-50%, 0%) scale(1)`;
-    token.style.transform = `translate(-50%, 0%) scale(0)`;
-  }
-
-  function firstVerticalScroll() {
-    if (isMediumScreen) {
-      hideSideBoxes();
-    }
-    sliders.forEach(slider => {
-      slider.style.transform = `translateX(0px)`;
-    });
-    container.style.top = `-${scrollY}px`;
-  }
-
-  function horizontalScroll() {
-    if (isMediumScreen) {
-      revealSideBoxes();
-    }
-    box.style.backgroundImage = backgroundGradients.red;
-    sliders.forEach(slider => {
-      slider.style.transform = `translateX(-${scrollY - firstScrolled}px)`;
-    });
-  }
-
-  function secondVerticalScroll() {
-    if (currentBox >= 6) {
-      revealTokens();
-    } else {
-      hideTokens();
-    }
-    if (isMediumScreen) {
-      disappearSideBoxes();
-    }
-    box.style.backgroundImage = backgroundGradients.green;
-    container.style.top = `-${scrollY - (containerWidth * 2)}px`;
-  }
-
-  function animateImages() {
-    if (currentBox >= 4) {
-      secondVerticalScroll()
-    } else if (currentBox >= 2 && currentBox < 4) {
-      horizontalScroll()
-    } else if (currentBox <= 1) {
-      firstVerticalScroll()
-    }
-  }
-
-  function rotateTitles() {
-    titleContainer.style.top = `-${(currentBox - 1) * titleHeight}px`;
-  }
-
-  function initBullet() {
-    bullets[0].classList.add('w--current');
-  }
-
-  function toggleBullets() {
-    bullets.forEach((bullet, index) => {
-      bullet.classList.toggle('w--current', currentBox === index + 1);
-    });
-  }
-
-  function bulletClick() {
-    bullets.forEach((bullet, index) => {
-      bullet.addEventListener('click', () => {
-        window.scrollTo({
-          top: scrollMap[index + 1],
-          behavior: 'smooth'
-        });
-      });
-    });
-  }
-
-  function onScroll() {
-    scrollY = window.scrollY;
-
-    for (let box of sortedScrollMap) {
-      if (scrollY >= scrollMap[box]) {
-        currentBox = parseInt(box);
-        break;
+    animating = true;
+    const tl = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+      onComplete: () => {
+        animating = false
+        delay = Date.now()
       }
-    }
+    });
 
-    animateImages()
-    rotateTitles()
-    toggleBullets()
+    tl.to(window, { scrollTo: { y: sections[index].offsetTop } });
+
+    currentIndex = index;
   }
 
-  initBullet()
-  bulletClick()
-  window.addEventListener('scroll', onScroll);
+  Observer.create({
+    wheelSpeed: -1,
+    tolerance: 10,
+    preventDefault: true,
+    onDown: () => gotoSection(currentIndex - 1),
+    onUp: () => gotoSection(currentIndex + 1),
+  });
+
+  gotoSection(0);
 }
 
 window.Webflow?.push(() => {
-  safeExecute(animateFundamental);
+  safeExecute(fullPageScroll);
 });
