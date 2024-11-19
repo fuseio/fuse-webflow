ad91d7bfb412473a5392095330153592a0d099e9
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-11-19 11:34:06 +0000

    Add drag in Fuse image manifesto

--- src/scripts/manifesto.js
+++ src/scripts/manifesto.js
@@ -9,11 +9,17 @@ function fullPageScroll() {
   let delay = 0
 
   const bullets = document.querySelectorAll('.manifesto_hero-bullet');
+  const fuse = document.querySelector('.manifesto_essential-image.is-fuse');
+  const fuseHeader = document.querySelector('.manifesto_essential-header-fuse');
+  const partners = document.querySelector('.manifesto_essential-partners');
 
   function gotoSection(index) {
     if (index < 0 || index >= sections.length || animating || !sections[index] || Date.now() - delay < 400) return;
 
     animating = true;
+    const isB2b2c = index === sections.length - 3;
+    const isNetwork = index === sections.length - 2;
+
     const tl = gsap.timeline({
       defaults: { duration: 1, ease: "power2.inOut" },
       onComplete: () => {
@@ -22,7 +28,18 @@ function fullPageScroll() {
       }
     });
 
-    tl.to(window, { scrollTo: { y: sections[index].offsetTop } });
+    const scrollTo = window.scrollY + sections[index].getBoundingClientRect().top;
+    tl.to(window, { scrollTo: { y: scrollTo } });
+    
+    if (isNetwork) {
+      gsap.to(fuse, { scale: 0.9 });
+      gsap.to(fuseHeader, { y: '-13rem' });
+      gsap.to(partners, { scale: 1, x: '-50%', y: '-50%' });
+    } else if (isB2b2c) {
+      gsap.to(fuse, { scale: 1 });
+      gsap.to(fuseHeader, { y: '0rem' });
+      gsap.to(partners, { scale: 0, x: '-50%', y: '-50%' });
+    }
 
     currentIndex = index;
   }
@@ -38,7 +55,7 @@ function fullPageScroll() {
   gotoSection(0);
 
   bullets.forEach((bullet, index) => {
-    bullet.addEventListener('click', () => currentIndex = index);
+    bullet.addEventListener('click', () => gotoSection(index));
   });
 }
 
