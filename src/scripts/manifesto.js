4b3410bb02feaee81c4066565ebf3b017280c426
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-11-20 12:02:07 +0000

    Add arrow pagination and Node Sale hover

--- src/scripts/manifesto.js
+++ src/scripts/manifesto.js
@@ -4,21 +4,23 @@ function fullPageScroll() {
   gsap.registerPlugin(Observer, ScrollToPlugin);
 
   const sections = document.querySelectorAll('.main-wrapper .container section');
-  let currentIndex = 0;
-  let animating = false;
-  let delay = 0
-
   const bullets = document.querySelectorAll('.manifesto_hero-bullet');
+  const arrow = document.querySelector('.manifesto_pagination-arrow');
   const fuse = document.querySelector('.manifesto_essential-image.is-fuse');
   const fuseHeader = document.querySelector('.manifesto_essential-header-fuse');
   const partners = document.querySelector('.manifesto_essential-partners');
+
+  let currentIndex = 0;
+  let animating = false;
+  let delay = 0;
+  const infoIndex = sections.length - 1;
 
   function gotoSection(index) {
     if (index < 0 || index >= sections.length || animating || !sections[index] || Date.now() - delay < 400) return;
 
     animating = true;
-    const isB2b2c = index === sections.length - 3;
     const isNetwork = index === sections.length - 2;
+    const isB2b2c = index === sections.length - 3;
 
     const tl = gsap.timeline({
       defaults: { duration: 1, ease: "power2.inOut" },
@@ -32,10 +34,12 @@ function fullPageScroll() {
     tl.to(window, { scrollTo: { y: scrollTo } });
     
     if (isNetwork) {
+      gsap.to(arrow, { opacity: 1, y: 0 });
       gsap.to(fuse, { scale: 0.9 });
       gsap.to(fuseHeader, { y: '-13rem' });
       gsap.to(partners, { scale: 1, x: '-50%', y: '-50%' });
     } else if (isB2b2c) {
+      gsap.to(arrow, { opacity: 0, y: -10 });
       gsap.to(fuse, { scale: 1 });
       gsap.to(fuseHeader, { y: '0rem' });
       gsap.to(partners, { scale: 0, x: '-50%', y: '-50%' });
@@ -57,6 +61,7 @@ function fullPageScroll() {
   bullets.forEach((bullet, index) => {
     bullet.addEventListener('click', () => gotoSection(index));
   });
+  arrow.addEventListener('click', () => gotoSection(infoIndex));
 }
 
 window.Webflow?.push(() => {
