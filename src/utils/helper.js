4bb5a3e27159ffb1b3cb855c90f4a9f335513e30
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2025-01-07 16:19:29 +0000

    Update uptime digit in animateHeroNumbers

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -24,8 +24,8 @@ export function animateHeroNumbers() {
       target: document.querySelector("#uptime-digit"),
       defaults: {
         startVal: 0,
-        endVal: 99.99,
-        decimalPlaces: 2,
+        endVal: 9000,
+        decimalPlaces: 0,
         duration: 2,
       },
     },
